fc09615f2ffe2d900eccf9eefadb78c89f56cd6f
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-04-05 18:46:47 +0300

    sum compare array and target algorithm lab

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  
+//   // Write your algorithm here
+for (let i=0;i<array.length;i++){
+/* a + b = c ; we have c = target
+first iteration to find a = array[i]
+*/ 
+  const b = target - array[i]
+  /*
+   second iteration to start at index 1 so it doesn't compare
+  against itself on the conditonal statement
+  */
+  for (let num = i+1; num<array.length; num++) {
+    if(array[num]===b)return true
+    } 
 }
+return false
+}
+
 
 /* 
   Write the Big O time complexity of your function here
+first iteration O(n)
+const declaration O(1)
+second iteration O(n)
+conditional statement O(1)
 */
 
 /* 
   Add your pseudocode here
+    sum all numbers in the array to each other, recurse
+    compare the numbers to the other argument and return true or false
 */
 
 /*
